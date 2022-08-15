b4477d4695a19e5e8014cc69817f6a63922ad37f
OlgaT <oliunea-t2002@mail.ru>
2022-08-16 00:13:11 +0300

    add zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+// например: 3 -> 1,8,27. 5 -> 1,8,27,64,125.
+Console.WriteLine("Введите число: ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+int index = 1;
+if(number<=0)
+{
+    Console.WriteLine("Введено некорректное число.");
+}
+while(number>=index)
+{
+    var result = Math.Pow(index,3);
+    index++;
+    Console.WriteLine(result);
+}
